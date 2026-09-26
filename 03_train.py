b1897d2c1886ff3f1e0aff6350df97a881ca0488
amill111 <amemillard@gmail.com>
2026-09-26 10:40:32 -0700

    Train for 100 effective epochs to match paper

--- 03_train.py
+++ 03_train.py
@@ -29,7 +29,7 @@ output_dir = Path("weights")
 
 num_splits = 4
 strategy = "X:1"
-epochs = 200
+epochs = 400
 # NOTE: reduce the batch size to fit training in GPU memory for unet and dncnn
 batch_size = 2
 multi_gpu = False
